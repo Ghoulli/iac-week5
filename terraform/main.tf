f169dc9eb049661fbd5bba87fc54076106ebbd7b
Ghoulli <58692950+Ghoulli@users.noreply.github.com>
2025-10-02 14:11:35 +0200

    123

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     esxi = {
-      source = "registry.terraform.io/josenk/esxi"
+      source  = "josenk/esxi"
+      version = ">= 1.10.0"
     }
   }
 }
